cfa889365d072327b96f16d6779ede52bd9928be
jorgelainfiesta <jorge.lainfiesta@gmail.com>
2016-12-08 11:09:50 -0600

    Header and terminal styles.

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,10 @@ module.exports = function(environment) {
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
+    googleFonts: [
+      'Karla:400,700',
+      'PT+Mono'
+    ],
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
