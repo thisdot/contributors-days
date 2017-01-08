1386a6988e26b6256949bd74c8151e77352c6988
Taras Mankovski <tarasm@gmail.com>
2017-01-08 14:23:41 -0500

    Adding secret config file

--- server/server.js
+++ server/server.js
@@ -3,9 +3,16 @@
 const GCloudStorageDownloader = require('fastboot-gcloud-storage-downloader');
 const GCloudStorageNotifier   = require('fastboot-gcloud-storage-notifier');
 const FastBootAppServer       = require('fastboot-app-server');
+const RedisCache              = require('fastboot-redis-cache');
+const nconf                   = require('nconf');
 
-const GCS_BUCKET = process.env.FASTBOOT_GCS_BUCKET;
-const GCS_KEY    = process.env.FASTBOOT_GCS_KEY;
+nconf.file({ file: 'config.json' });
+
+const GCS_BUCKET = nconf.get('bucket');
+const GCS_KEY    = nconf.get('key');
+const FASTBOOT_REDIS_HOST = nconf.get('redis_host');
+const FASTBOOT_REDIS_PORT = nconf.get('redis_port');
+const FASTBOOT_REDIS_PASSWORD = nconf.get('redis_password');
 
 let downloader = new GCloudStorageDownloader({
   bucket: GCS_BUCKET,
@@ -17,9 +24,19 @@ let notifier  = new GCloudStorageNotifier({
   key: GCS_KEY
 });
 
+let cache = new RedisCache({
+  host: FASTBOOT_REDIS_HOST,
+  port: FASTBOOT_REDIS_PORT,
+  password: FASTBOOT_REDIS_PASSWORD,
+  cacheKey(path) {
+    return `contributor-days-${path}`;
+  }
+});
+
 let server = new FastBootAppServer({
   downloader,
-  notifier
+  notifier,
+  cache
 });
 
 server.start();
