bb255093efb62393ebb3770605aabb2f6646c9ef
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2017-04-24 16:46:21 -0600

    Signup form on contributor days page

--- app/controllers/contributor-day.js
+++ app/controllers/contributor-day.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
+import moment from 'moment';
 import recordsById from '../utils/records-by-id';
+const { computed } = Ember;
 
 export default Ember.Controller.extend({
   attendees: recordsById('model.attendees', 'author'),
-  leads: recordsById('model.leads', 'author')
+  leads: recordsById('model.leads', 'author'),
+  isPast: computed('model.date', function() {
+    let today = moment();
+    let eventDate = moment(this.get('model.date'));
+    return today.isAfter(eventDate, 'day');
+  }),
+  isFuture: computed.not('isPast'),
 });
