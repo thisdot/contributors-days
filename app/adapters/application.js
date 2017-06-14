906a18a141c4f56e2c234c2a83aa333f231665a4
Taras Mankovski <tarasm@gmail.com>
2017-06-13 22:36:24 -0400

    Fixed host name

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,11 @@
 import ApplicationAdapter from 'ember-writer/adapters/application';
+import config from '../config/environment';
+
+const {
+  host
+} = config;
 
 export default ApplicationAdapter.extend({
+  host,
   namespace: 'api/blog'
 });
